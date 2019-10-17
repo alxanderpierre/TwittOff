f5250df8d16d5488fac488f4c197932f9e49f8e5
alxanderpierre <alxanderpierre@users.noreply.github.com>
2019-10-16 20:12:48 -0400

    test

---
+++ twitoff/app.py
@@ -0,0 +1,43 @@
+"""Main application for twitoff"""
+
+# imports
+from decouple import config
+from flask import Flask, render_template, request
+from .model import DB, User
+from .twitter import add_or_update_user
+
+
+def create_app():
+    """create and configures an instance of a flask app"""
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/user', methods=['POST','GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None,message=''):
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added!".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets=[]
+        return render_template('user.html', title=name, tweets=tweets,
+        message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset', users=[])
+    return app
